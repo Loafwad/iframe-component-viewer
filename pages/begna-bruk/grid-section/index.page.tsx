e2286d0f067a5582325f159db33d3e5ab878048f
Nathaniel Brandal <nathbrandal@gmail.com>
2022-09-30 15:51:28 +0200

    added info label to component

--- pages/begna-bruk/grid-section/index.page.tsx
+++ pages/begna-bruk/grid-section/index.page.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiX } from "react-icons/hi";
 import { Spacer } from "../../../components/Misc/Spacer";
+import InfoLabel from "../../../components/Story/InfoLabel";
 import { GridSection } from "./GridSection";
 
 const data = {
@@ -68,14 +70,41 @@ const data = {
   },
 };
 
-const index = () => {
+const Index = () => {
+  function CompProps() {
+    const [first, setfirst] = useState(false);
+
+    function handleClose() {
+      setfirst(!first);
+      console.log("click");
+    }
+    return (
+      <div className="bg-white left-0 bottom-0 fixed m-12 rounded-md shadow-md z-20">
+        <button className={`mb-auto p-6 overflow-hidden relative text-black  text-2xl`} onClick={handleClose}>
+          <div className="flex gap-2">
+            <HiX />
+            <p className="text-sm my-auto ">Props</p>
+          </div>
+        </button>
+        <div className={` flex overflow-hidden mr-auto flex-col gap-6 ${first ? "h-0 p-0" : "h-full p-6"}`}>
+          <InfoLabel
+            text={`Component is hard coded to maxium of two rows.
+
+Will **error** if two rows are not provided.`}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <main>
       <Spacer />
+      <CompProps />
       <GridSection {...data} />
       <Spacer />
     </main>
   );
 };
 
-export default index;
+export default Index;
