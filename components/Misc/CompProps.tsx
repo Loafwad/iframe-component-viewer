d3cbf6fb261e28650cf44a93e519bc49ac45d27e
Nathaniel Brandal <nathbrandal@gmail.com>
2022-10-03 15:31:42 +0200

    removed draggable lol

--- components/Misc/CompProps.tsx
+++ components/Misc/CompProps.tsx
@@ -9,19 +9,17 @@ const CompProps = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
     setfirst(!first);
   }
   return (
-    <Draggable>
-      <div className="bg-white left-0 bottom-0 fixed m-2 md:m-12 rounded-md shadow-md z-20">
-        <button className={`mb-auto p-6 overflow-hidden relative text-black  text-2xl`} onClick={handleClose}>
-          <div className="flex gap-2">
-            <HiX />
-            <p className="text-sm my-auto ">Props</p>
-          </div>
-        </button>
-        <div className={` flex overflow-hidden mr-auto flex-col gap-6 ${first ? "h-0 p-0" : "h-full p-6"}`}>
-          {children}
+    <div className="bg-white left-0 bottom-0 fixed m-2 md:m-12 rounded-md shadow-md z-20">
+      <button className={`mb-auto p-6 overflow-hidden relative text-black  text-2xl`} onClick={handleClose}>
+        <div className="flex gap-2">
+          <HiX />
+          <p className="text-sm my-auto ">Props</p>
         </div>
+      </button>
+      <div className={` flex overflow-hidden mr-auto flex-col gap-6 ${first ? "h-0 p-0" : "h-full p-6"}`}>
+        {children}
       </div>
-    </Draggable>
+    </div>
   );
 };
 
