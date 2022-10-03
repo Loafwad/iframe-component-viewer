d6ee25372ee8659d45d10fec3cb3852f6123551c
Nathaniel Brandal <nathbrandal@gmail.com>
2022-10-03 15:24:51 +0200

    fixed bug

--- components/Story/PropArea.tsx
+++ components/Story/PropArea.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const PropArea = ({ state, setState, label }: { state: string; setState: any; label: string }) => {
   const [value, setValue] = useState(state);
   function handleValue(e: any) {
-    setValue(e.target.value);
     handleUpdateState();
+    setValue(e.target.value);
   }
+
+  useEffect(() => {
+    setState(value);
+  }, [setState, value]);
+
   function handleUpdateState() {
     setState(value);
   }
@@ -13,7 +18,7 @@ const PropArea = ({ state, setState, label }: { state: string; setState: any; la
     <div className="gap-2 flex ">
       <label>{label}</label>
       <textarea
-        placeholder={state}
+        placeholder={value}
         value={value}
         className="text-top w-full p-2 bg-black bg-opacity-5 rounded-md min-h-[12ch] max-h-full overflow-y-auto"
         onChange={(e) => handleValue(e)}
